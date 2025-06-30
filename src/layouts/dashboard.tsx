f6f12d5b00380bbdf367cdb2f4648933ff466199
chiranth2428 <chiranth@interactivedigitalmedia.in>
2025-06-30 15:19:41 +0530

    first commit

---
+++ src/layouts/dashboard.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface DashboardProps {}
+
+const Dashboard: React.FunctionComponent<DashboardProps> = () => {
+  return <>dashboard</>;
+};
+
+export default Dashboard;
